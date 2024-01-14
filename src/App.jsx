024de9d6f9e8de962e7375272105f55e8054b9cc
Barrett Anderson <barrett1614@gmail.com>
2024-01-14 11:14:53 -0600

    Preliminary Score Log created

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
