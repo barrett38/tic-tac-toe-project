6088c442c4c2bf9f645051af52addede13d307ae
Barrett Anderson <barrett1614@gmail.com>
2024-01-15 20:28:36 -0600

    Unneeded comments removed

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -3,6 +3,7 @@ export default function GameOver({ winner, onRestart }) {
     <div id="game-over">
       <h2>Game Over</h2>
       {winner && <p>{winner.toUpperCase()} WON!</p>}
+      {/* Changed to .toUpperCase() for more consistency */}
       {!winner && <p>It's a draw!</p>}
       <p>
         <button onClick={onRestart}>Rematch!</button>
